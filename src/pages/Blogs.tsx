9f6f435b3d635affe7e8f3695ea26c1967f74339
zahidhasanemran <zahid.emran@thealtcom.com>
2024-03-17 03:26:33 -0400

    blog page and single blog post page api fetch done

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { createResource, For, Match, Show } from 'solid-js'
+
+const fetchPosts = async () =>{
+  const response = await fetch(`https://dummyjson.com/posts?limit=10`);
+  const res = await response.json();
+  // console.log(res);
+  
+  return res;
+}
+
 
 const Blogs = () => {
+
+  const [products] = createResource(fetchPosts)
+
   return (
-    <div>
-      Blogs page 
+    <div class="px-8 py-8  ">
+      <Show when={products.loading}>
+        <h2>Loading ...</h2>
+      </Show>
+      <Match when={products.error}>
+        <h2>Error ...</h2>
+      </Match>
+      <Show when={products()}>
+        <div class="grid grid-cols-4 gap-10 my-4">
+        <For each={products().posts}>
+          {product => <a href={`/blog/${product.id}`} class="border block rounded px-4 py-4  ">
+            <h2 class="text-black text-xl ">{product.title}</h2>
+            </a>}
+        </For>
+        </div>
+      </Show>
     </div>
   );
 };
--- src/pages/SingleBlogPage.tsx
+++ src/pages/SingleBlogPage.tsx
@@ -1,12 +1,36 @@
 
 import { useParams } from '@solidjs/router'
-import React from 'react';
+import { createResource, Show, Switch, Match } from 'solid-js'
+
+const fetchPost = async (id: string) =>{
+  const response = await fetch(`https://dummyjson.com/post/${id}`);
+  const res = await response.json();
+  // console.log(res);
+  return res;
+}
 
 const SingleBlogPage = () => {
+
   const {id} = useParams();
+  const [user] = createResource(id, fetchPost)
+
   return (
     <div>
-      Single Blog post page {id}
+      <Show when={user.loading}>
+        <p>Loading...</p>
+      </Show>
+      <Switch>
+        <Match when={user.error}>
+          <span>Error:  </span>
+        </Match>
+        <Match when={user()}>
+          <div class='w-full max-w-[500px] px-5 py-5 mx-auto mt-12 border rounded shadow-sm border-blue-200 '>
+            <a class="inline-block text-white px-4 py-3 rounded-md mb-8 bg-primary-600 " href="/blog">  Back to Blog</a>
+            <h2 class="mb-4 text-2xl  ">{user().title}</h2>
+            <p>{user().body}</p>
+          </div>
+        </Match>
+      </Switch>
     </div>
   );
 };
